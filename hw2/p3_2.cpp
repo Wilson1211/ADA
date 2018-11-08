646f54c9504dcfdb26d40075ba4e3690ef667828
Wilson1211 <raito244@gmail.com>
2018-11-08 13:02:06 +0800

    sol of hw2

--- hw2/p3_2.cpp
+++ hw2/p3_2.cpp
@@ -3,26 +3,29 @@ using namespace std;
 
 typedef long long int Int;
 
+
+
 int main(){
 	Int n,m;
 	Int i, j ,k1,k2,k3,rem,k;
-	Int ******a = new Int*****[1000];//[10][1000][1000][1000][33];
-	for(i=0;i<1000){
-		*****a[i] = new Int****[10];
+	Int ******a = new Int*****[1000];//[10][1000][1000][1000][33]; [10][3][3][3][33]
+	for(i=0;i<1000;i++){
+		a[i] = new Int****[10];
 		for(j=0;j<10;j++){
-			****a[i][j]=new Int***[1000];
-			for(k1=0;k1<1000;k1++){
-				***a[i][j][k1]=new **Int[1000];
-				for(k2=0;k2<1000;k2++){
-					**a[i][j][k1][k2] = new *Int[1000];
-					for(k3=0;k3<1000;k3++){
-						*a[i][j][k1][k2][k3] = new Int[33];
+			a[i][j]=new Int***[3];//[1000];
+			for(k1=0;k1<3;k1++){
+				a[i][j][k1]=new Int**[3];//[1000];
+				for(k2=0;k2<3;k2++){
+					a[i][j][k1][k2] = new Int*[3];//[1000];
+					for(k3=0;k3<3;k3++){
+						a[i][j][k1][k2][k3] = new Int[33];
 					}
 				}
 			}
 		}
 	}
 	// first row
+
 	for(j=0;j<10;j++){
 		if(j==3){
 			a[0][j][1][0][0][j] = 1;
@@ -36,51 +39,25 @@ int main(){
 	}
 
 
+
+
 	for(i=1;i<1000;i++){
-		for(j=0;j<i;j++){
+		for(j=0;j<10;j++){
 			if(j==3){
-				for(k1=1;k1<=i+1;k1++){
-					for(k2=0;k2<=(i+1-k1);k2++){
-						for(k3=0;k3<=(i+1-k1-k2);k3++){
+				for(k1=0;k1<=2;k1++){
+					for(k2=0;k2<=2;k2++){	// i+1-k1
+						for(k3=0;k3<=2;k3++){	// i+1-k1-k2
 							for(rem=0;rem<33;rem++){								
 								for(k=0;k<10;k++){
-									a[i][j][k1][k2][k3][(rem+j)%33] += a[i-1][k][k1-1][k2][k3][rem];
-								}								
-							}
-						}
-					}
-				}
-			}else if(j==6){
-				for(k1=0;k1<=i+1;k1++){
-					for(k2=1;k2<=(i+1-k1);k2++){
-						for(k3=0;k3<=(i+1-k1-k2);k3++){
-							for(rem=0;rem<33;rem++){			
-								for(k=0;k<10;k++){
-									a[i][j][k1][k2][k3][(rem+j)%33] += a[i-1][k][k1][k2-1][k3][rem];
-								}
-							}
-						}
-					}
-				}
-			}else if(j==9){
-				for(k1=0;k1<=i+1;k1++){
-					for(k2=0;k2<=(i+1-k1);k2++){
-						for(k3=1;k3<=(i+1-k1-k2);k3++){
-							for(rem=0;rem<33;rem++){
-								for(k=0;k<10;k++){
-									a[i][j][k1][k2][k3][(rem+j)%33] += a[i-1][k][k1][k2][k3-1][rem];
-								}								
-							}
-						}
-					}
-				}
-			}else{
-				for(k1=0;k1<=i+1;k1++){
-					for(k2=0;k2<=(i+1-k1);k2++){
-						for(k3=0;k3<=(i+1-k1-k2);k3++){
-							for(rem=0;rem<33;rem++){{
-								for(k=0;k<10;k++){
-									a[i][j][k1][k2][k3][(rem+j)%33] += a[i-1][k][k1][k2][k3][rem];
+									if(j==3){
+										a[i][j][k1][k2][k3][rem] += a[i-1][k][(k1+2)%3][k2][k3][(rem+33-j)%33];
+									}else if(j==6){
+										a[i][j][k1][k2][k3][rem] += a[i-1][k][k1][(k2+2)%3][k3][(rem+33-j)%33];
+									}else if(j==9){
+										a[i][j][k1][k2][k3][rem] += a[i-1][k][k1][k2][(k3+2)%3][(rem+33-j)%33];
+									}else{
+										a[i][j][k1][k2][k3][rem] += a[i-1][k][k1][k2][k3][(rem+33-j)%33];
+									}
 								}
 							}
 						}
@@ -90,9 +67,102 @@ int main(){
 		}
 	}
 
+/*
+
+
+	for(i=1;i<1000;i++){
+		for(j=0;j<10;j++){
+			if(j==3){
+				for(k1=0;k1<=2;k1++){
+					for(k2=0;k2<=2;k2++){	// i+1-k1
+						for(k3=0;k3<=2;k3++){	// i+1-k1-k2
+							for(rem=0;rem<33;rem++){								
+								for(k=0;k<10;k++){
+									a[i][j][k1][k2][k3][rem] += a[i-1][k][(k1+2)%3][k2][k3][(rem+33-j)%33];
+								}								
+							}
+						}
+					}
+				}
+			}else if(j==6){
+				for(k1=0;k1<=2;k1++){
+					for(k2=0;k2<=2;k2++){	// i+1-k1
+						for(k3=0;k3<=2;k3++){	// i+1-k1-k2
+							for(rem=0;rem<33;rem++){			
+								for(k=0;k<10;k++){
+									a[i][j][k1][k2][k3][rem] += a[i-1][k][k1][(k2+2)%3][k3][(rem+33-j)%33];
+								}
+							}
+						}
+					}
+				}
+			}else if(j==9){
+				for(k1=0;k1<=2;k1++){
+					for(k2=0;k2<=2;k2++){
+						for(k3=0;k3<=2;k3++){	// i+1-k1-k2
+							for(rem=0;rem<33;rem++){
+								for(k=0;k<10;k++){
+									a[i][j][k1][k2][k3][rem] += a[i-1][k][k1][k2][(k3+2)%3][(rem+33-j)%33];
+								}								
+							}
+						}
+					}
+				}
+			}else{
+				for(k1=0;k1<=2;k1++){
+					for(k2=0;k2<=2;k2++){	// i+1-k1
+						for(k3=0;k3<=2;k3++){	// i+1-k1-k2
+							for(rem=0;rem<33;rem++){
+								for(k=0;k<10;k++){
+									a[i][j][k1][k2][k3][rem] += a[i-1][k][k1][k2][k3][(rem+33-j)%33];
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+*/
 	cin >> n >> m;
+	Int sum1=0,sum2=0;
+	int size=0;
+	rem=0;
+	int b1[1000]={};
+	int b2[1000]={};
+	n--;// since l to r include l
+	if(n>1){
+		while(n!=0){
+			k = n%10;
+			b1[size]=k;
+			size++;
+			n/=10;
+		}
+		for(i=size-1;i>=0;i--){
+			for(j=0;j<b1[i];j++){
+				sum1+=a[i][j][0][0][0][0];
+			}
+		}
+		sum1+=a[0][b1[0]][0][0][0][0];
 
-	
 
+	}
+	size=0;
+	while(m!=0){
+		k=m%10;
+		b2[size] = k;
+		size++;
+		m/=10;
+	}
+	for(i=size-1;i>=0;i--){
+		for(j=0;j<b2[i];j++){
+			sum2+=a[i][j][0][0][0][0];
+		}
+	}
+	sum2+=a[0][b2[0]][0][0][0][0];
+
+	cout<<sum2-sum1<<endl;
+
+	return 0;
 
 }
